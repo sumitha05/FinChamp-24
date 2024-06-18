c712e6de2d5ca2663b340bdeeaa4b0e3ec0840a3
Sumitha Sreekumar <sumisree502@gmail.com>
2024-06-18 11:09:11 +0530

    Update sidebar.tsx

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -18,14 +18,14 @@ export const Sidebar = ({className}: Props) => {
         <div className={cn("flex h-full lg:w-[256px] lg:fixed left-0 top-0 px-4 border-r-2 flex-col",
             className,
         )}>
-            <Link href="/student">
+            
             <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
                 <Image src="/icon.png" height={40} width={40} alt="Mascot"/>
                 <h1 className="text=2xl font-extrabold text-indigo-950 tracking-wide">
                     FinChamp
                 </h1>
             </div>
-            </Link>
+            
          <div className="flex flex-col gap-y-2 flex-1">
             <SidebarItem 
             label="Home" href="/home-student" iconSrc="/home.png"
